f582a883c6b50023041f0546b5524f0d08cb9466
Catalin Giurgiu <catalingiurgiu995@yahoo.com>
2017-04-16 18:49:38 +0000

    removed unnecessary files

--- Solution/invert.py
+++
@@ -1,6 +0,0 @@
-def invert (lista):
-	nlist = []
-	for x in lista:
-		if lista[x] % 2 == 1:
-			nlist.append(-1 * lista[x])
-	return nlist
--- Solution/invert2.py
+++
@@ -1,2 +0,0 @@
-def invert2(lista):
-	return [-1 * item for item in lista]
